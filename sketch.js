f1b1f9abf6a0dffbb08dbaedf2eb3f245d4562a7
sbongs <45353825+sbongs@users.noreply.github.com>
2018-11-27 01:23:37 +0900

    Update sketch.js
    make stars using list

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 'use strict'
 
+let stars = [];
+let speed;
+
 function setup(){
   createCanvas(640, 480);
+  for (let i =0; i < 800; i ++){
+    stars[i] = new Star();    
+  }
 }
 
 function draw(){
